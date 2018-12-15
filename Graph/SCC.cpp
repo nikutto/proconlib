a955e530cad4733627b18fb429ba8a9334a5597e
nikutto <nikutto@outlook.com>
2018-12-15 12:11:27 +0900

    verify SCC.cpp

--- Graph/SCC.cpp
+++ Graph/SCC.cpp
@@ -61,3 +61,34 @@ namespace ProconLib{
     }
 
 }
+
+
+// // verify AOJ(GRL/GRL_3_C)(http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_3_C)
+// struct Edge{
+//     int to;
+// };
+// using namespace std;
+// using namespace ProconLib;
+// using Graph=vector<vector<Edge>>;
+
+// int main(){
+//     int V,E;
+//     cin>>V>>E;
+//     Graph g(V);
+//     for(int i=0;i<E;i++) {
+//         int u,v;
+//         cin>>u>>v;
+//         g[u].push_back(Edge{v});
+//     }
+//     SCC<Graph> scc(g);
+//     int q;
+//     cin>>q;
+//     auto cmp=scc.getCmp();
+//     while(q--){
+//         int u,v;
+//         cin>>u>>v;
+//         cout<<(cmp[u]==cmp[v])<<endl;
+//     }
+//     return 0;
+// }
+//
