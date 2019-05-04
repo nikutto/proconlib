bd86e51dc6ea020d00dd588a17df91e755e12f93
nikutto <nikutto@outlook.com>
2019-05-04 10:31:31 +0900

    Some cleaning

---
+++ Template/tmp.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cassert>
+#include<stdexcept>
+#include<utility>
+#include<functional>
+#include<numeric>
+#include<cmath>
+#include<algorithm>
+#include<cstdio>
+#include<cstdlib>
+#include<array>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<vector>
+#include<complex>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<string>
+#include<bitset>
+using namespace std;
+using ll=long long;
+
+int main(){
+    
+    
+    return 0;
+}
