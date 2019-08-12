87089fef558975e38153c9367d749a95a05153b9
nikutto <nikuto@outlook.com>
2019-08-12 09:41:25 +0900

    Add polynomial

---
+++ Math/Polynomial.cpp
@@ -0,0 +1,104 @@
+#include<vector>
+#include<algorithm>
+namespace ProconLib{
+
+    namespace DetailNS{
+        template<typename T>
+        struct DefaultDetail{
+            static bool isZero(T x){
+                return x<=T(0);
+            }
+        };
+    }
+    template<typename T>
+    class Polynomial{
+        int N;
+        std::vector<T> dat;
+        
+        public:
+        Polynomial(int N):N(N),dat(N+1){}
+
+        int degree(){return N;}
+        void resize(int N){ this->N=N; dat.resize(N+1,T(0));}
+        template<typename Detail=DetailNS::DefaultDetail<T>>
+        void shrink();
+        T& operator[](int pos){return dat[pos];}
+        const T& operator[](int pos) const {return dat[pos];}
+    };
+
+    template<typename T>
+    Polynomial<T>& operator+=(Polynomial<T> &lhs,Polynomial<T> rhs);
+    template<typename T>
+    Polynomial<T>& operator-=(Polynomial<T> &lhs,Polynomial<T> rhs);
+    template<typename T>
+    Polynomial<T>& operator*=(Polynomial<T> &lhs,Polynomial<T> rhs);
+    template<typename T>
+    Polynomial<T> operator+(Polynomial<T> lhs,Polynomial<T> rhs){return lhs+=rhs;}
+    template<typename T>
+    Polynomial<T> operator-(Polynomial<T> lhs,Polynomial<T> rhs){return lhs-=rhs;}
+    template<typename T>
+    Polynomial<T> operator*(Polynomial<T> lhs,Polynomial<T> rhs){return lhs*=rhs;}
+
+    template<typename T>
+    template<typename Detail>
+    void Polynomial<T>::shrink(){ 
+        while(N){
+            if(Detail::isZero(dat[N])) N--;
+            else break;
+        }
+        resize(N);
+    }
+
+    template<typename T>
+    Polynomial<T>& operator+=(Polynomial<T> &lhs,Polynomial<T> rhs){
+        lhs.resize(std::max(lhs.degree(),rhs.degree()));
+        for(int i=0;i<=rhs.degree();i++) lhs[i]+=rhs[i];
+        return lhs;
+    }
+
+    template<typename T>
+    Polynomial<T>& operator-=(Polynomial<T> &lhs,Polynomial<T> rhs){
+        lhs.resize(std::max(lhs.degree(),rhs.degree()));
+        for(int i=0;i<=rhs.degree();i++) lhs[i]-=rhs[i];
+        return lhs;
+    }
+
+
+    template<typename T>
+    Polynomial<T>& operator*=(Polynomial<T> &lhs,Polynomial<T> rhs){
+        Polynomial<T> lhsTmp=lhs;
+        lhs.resize(lhs.degree()+rhs.degree());
+        for(int i=0;i<=lhsTmp.degree();i++) lhs[i]=T(0);
+        for(int i=0;i<=lhsTmp.degree();i++){
+            for(int j=0;j<=rhs.degree();j++){
+                lhs[i+j]+=lhsTmp[i]*rhs[j];
+            }
+        }
+        return lhs;
+    }
+
+    template<typename T>
+    Polynomial<T> operator+(Polynomial<T> poly){
+        auto res=poly;
+        return res;
+    }
+
+    template<typename T>
+    Polynomial<T> operator-(Polynomial<T> poly){
+        auto res=poly;
+        for(int i=0;i<=poly.degree();i++) res[i]=-res[i];
+        return res;
+    }
+
+    template<typename T>
+    T operator()(Polynomial<T> poly,T x){
+        T res=T(0);
+        T px=T(1);
+        for(int i=0;i<=poly.degree();i++){
+            res+=poly[i]*px;
+            px*=x;
+        }
+        return res;
+    }
+
+}
---
+++ Util/submitFormatter.py
@@ -0,0 +1,56 @@
+import sys
+argvs = sys.argv
+argc = len(argvs)
+
+def help():
+    desp = "usage: python3 compFormatter.py <file>\n"
+    sys.stderr.write(desp)
+
+if argc==1:
+    help()
+    exit(0)
+
+if argc>2:
+    desp = "Confirm usage. See 'python3 compFormatter.py --help'.\n"
+    sys.stderr.write(desp)
+    exit(1)
+
+if argc==2 and argvs[1]=="--help":
+    help()
+    exit(0)
+
+target_name = argvs[1]
+
+
+try:
+    with open(target_name) as tf:
+        target_lines = tf.readlines()
+except:
+    desp = "Failed to open file.\n"
+    sys.stderr.write(desp)        
+    exit(1)
+
+import re
+def expand(line):
+    pattern = r'#include"*"'
+    content = line[:-1]
+    m = re.match(pattern,content)
+    if not m: return line
+    lib_name= (content[9:-1])
+    try:
+        with open(lib_name) as lib:
+            ret=lib.read()
+            desp = "Sucess : Expanded '" + content +"'.\n"
+            sys.stderr.write(desp)
+            ret+="\n"
+            return ret
+    except:
+        desp = "Warning : Failed to expand '" + content +"'.\n" 
+        sys.stderr.write(desp)
+        return line
+
+res = ""
+for line in target_lines:
+    res+=expand(line)
+
+print(res)
